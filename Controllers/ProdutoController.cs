4b86767fb37d6198ed7c4d1e15ad06c818b45cf6
alexa <alexa@LAPTOP-UF0HUGSI>
2024-03-13 01:37:53 -0300

    Add project files.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Revisao_Aula7_MVC.Models;
+
+namespace Revisao_Aula7_MVC.Controllers
+{
+	public class ProdutoController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var produtos = new[]
+			{
+				new Produto { Id = 1, Nome = "Caneta", Preco = 2 },
+				new Produto { Id = 2, Nome = "Lápis", Preco = 1 },
+				new Produto { Id = 3, Nome = "Caderno", Preco = 10 }
+			}.ToList();
+
+			return View(produtos);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+//Adiciona middleware para redirecionar solicitações HTTP para HTTPS.
+app.UseHttpsRedirection();
+
+//Os arquivos são servidos a partir do caminho especificado em
+//Microsoft.AspNetCore.Hosting.IWebHostEnvironment.WebRootPath pasta wwwroot
+app.UseStaticFiles();
+
+//define um ponto no pipeline de middleware onde as decisões de roteamento
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Adiciona middleware para definir as rotas da aplicação
+app.MapControllerRoute(
+    name: "defualt",
+    pattern: "{controller=Produto}/{action=Index}/{id?}");
+
+app.Run();
